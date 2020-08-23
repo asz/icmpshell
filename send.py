0d3e4f9b0342a403a45dd046ff900a0b2cb35a20
asz <70036118+asz@users.noreply.github.com>
2020-08-23 17:31:19 +0300

    Script for crafting and sending ICMP packets with custom payload

---
+++ send.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import sys
+from scapy.all import sr1, IP, ICMP
+
+if len(sys.argv) < 3:
+    print('Usage: {} IP "command"'.format(sys.argv[0]))
+    exit(0)
+
+p = sr1(IP(dst=sys.argv[1])/ICMP()/sys.argv[2])
+if p:
+    p.show()
